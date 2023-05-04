9329a6d2f110a611eb9c7ce6ed2090e323c4f13f
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-05-04 17:01:16 +0430

    The comment for dashboard.py was included in project.

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,1 +1,26 @@
-The thest
+# Charts Page
+#
+# This data is used to illustrate the statistical data in the first page of the website.
+import json
+
+import pandas as pd
+
+# The data is used in this project after being cleaned and recreated from the original file. Final accident points
+# are collected data from Dec 2021 to Aug 2022 which is included the geographic and statistics data. This data is
+# allowed to show as a geographical geometry or illustrated as a list of data. As a result, the final file also is
+# provided the capability to create an analytic tool.
+df = pd.read_csv("../data/final_accident_points.csv")
+
+# The total number of accidents are calculated through the count of a column (OBJECTID) of the read CSV file.
+total_Accident = df.OBJECTID.count()
+
+# The total number of accidents are calculated through the sum up of KilledNumber column of the read CSV file,
+# this column is also included as an integer number in the field.
+total_killed_number = df.KilledNumber.sum()
+total_injured_number = df.InjuredNumber.sum()
+
+data = json.dumps({
+    "total_Accident": int(total_Accident),
+    "total_killed_number": int(total_killed_number),
+    'total_injured_number': int(total_injured_number)
+})
